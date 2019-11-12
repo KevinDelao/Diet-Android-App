6c83e42bbbd8bbfff21af0538f4d095e76e76e56
Prowtonz <38545753+Prowtonz@users.noreply.github.com>
2019-11-11 20:42:05 -0800

    Updated MainActivity.

--- app/src/main/java/com/example/dietapp/MainActivity.kt
+++ app/src/main/java/com/example/dietapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.dietapp
-
-import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.ui.NavigationUI
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.dietingapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
