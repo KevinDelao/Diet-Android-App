03e06dcf5bae1d151b293bbd4a90031537ea77a2
Kevin <Kevin@Kevin.com>
2019-11-12 18:05:07 -0800

    changed calorie layout

--- app/src/main/java/com/example/dietapp/CalorieFragment.kt
+++ app/src/main/java/com/example/dietapp/CalorieFragment.kt
@@ -8,18 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class CalorieFragment : Fragment() {
+    lateinit var v:View;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+    ): View?
+    {
+        v = inflater.inflate(R.layout.calorie_layout,container,false);
+
+        return v;
     }
 
 
