0e5559c4f0956d56e263a362f96f8408017fda56
Kevin <Kevin@Kevin.com>
2019-11-13 19:39:56 -0800

    made changes to calorie fragment

--- app/src/main/java/com/example/dietapp/CalorieFragment.kt
+++ app/src/main/java/com/example/dietapp/CalorieFragment.kt
@@ -6,21 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 
 
 class CalorieFragment : Fragment() {
     lateinit var v:View;
-
+    lateinit var addFood:Button;
+    lateinit var displayFood:TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View?
     {
         v = inflater.inflate(R.layout.calorie_layout,container,false);
+        addFood = v.findViewById(R.id.addFoodButton);
+        displayFood = v.findViewById(R.id.displayFoodCalorie);
+        addFood.setOnClickListener { calculateFood(it)}
 
         return v;
     }
+    private fun calculateFood(view: View)
+    {
+        displayFood.visibility = View.VISIBLE;
 
+    }
 
 }
--- app/src/main/java/com/example/dietapp/TitleFragment.kt
+++ app/src/main/java/com/example/dietapp/TitleFragment.kt
@@ -6,21 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.findNavController
 
 /**
  * A simple [Fragment] subclass.
  */
-class TitleFragment : Fragment() {
+class TitleFragment : Fragment()
+{
+    lateinit var viewTitle:View;
+    lateinit var toCaloriePage: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
+        viewTitle = inflater.inflate(R.layout.fragment_title,container,false);
+        toCaloriePage = viewTitle.findViewById(R.id.title_page_calorie_counter_button);
+
+        toCaloriePage.setOnClickListener{ view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_calorieFragment)
         }
-    }
-
-
+        return viewTitle
+        }
 }
+
+
+
