27926c6f60b85f3469474c763757ff1781dd2c55
KuongT <57548270+KuongT@users.noreply.github.com>
2019-12-14 01:09:07 -0800

    local
    changed string names

---
+++ BindingUtils.kt
@@ -0,0 +1,13 @@
+package com.example.dietapp.util
+
+import android.text.format.DateUtils
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+
+@BindingAdapter("elapsedTime")
+fun TextView.setElapsedTime(value: Long)
+{
+    val seconds = value / 1000
+    text = if (seconds < 60) seconds.toString() else DateUtils.formatElapsedTime(seconds)
+}
